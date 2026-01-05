17e101e40c23ca199d81c7625b78e315435c8988
Ravi <ravi@Ravis-MacBook-Air.local>
2026-01-05 15:41:34 +0530

    Setup the project

---
+++ backend/src/index.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+}));
+
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('Ziksir Notes App Backend');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
